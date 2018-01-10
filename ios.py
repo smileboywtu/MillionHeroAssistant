5385a84f2e1ac926815157a0585a07d4a2573df2
uniqhj <747915050@qq.com>
2018-01-10 18:22:36 +0800

    baidu ocr

--- ios.py
+++ ios.py
@@ -4,25 +4,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import textwrap
-import time
+from functools import partial
 from PIL import Image
 from argparse import ArgumentParser
-from config import data_directory, hanwan_appcode
-from config import default_answer_number
-from core.baiduzhidao import zhidao_search
-from core.hanwanocr import get_text_from_image
 
-import urllib.request, sys,base64,json,os,time,pyperclip,baiduSearch
+from config import app_id
+from config import app_key
+from config import app_secret
+from config import data_directory
+from config import default_answer_number
+from config import hanwan_appcode
+from config import image_compress_level
+from config import prefer
+from config import summary_sentence_count
+from config import text_summary
+from core.baiduzhidao import zhidao_search
+from core.ocr.baiduocr import get_text_from_image as bai_get_text
+from core.ocr.hanwanocr import get_text_from_image as han_get_text
+from core.textsummary import get_summary
+
+
+if prefer[0] == "baidu":
+    get_text_from_image = partial(bai_get_text, app_id=app_id, app_key=app_key, app_secret=app_secret, timeout=5)
+elif prefer[0] == "hanwang":
+    get_text_from_image = partial(han_get_text, appcode=hanwan_appcode, timeout=3)
 
 c = wda.Client()
 s = c.session()
 
     
-def analyze_current_screen_text():
-    c.screenshot('screenshot.png')
-    img = np.array(Image.open('autojump.png'))
-    wide = img.size[0]
-    img = img.crop((70, 200, wide - 70, 700))
+# def analyze_current_screen_text():
+#     c.screenshot('screenshot.png')
+#     img = np.array(Image.open('autojump.png'))
+#     wide = img.size[0]
+#     img = img.crop((70, 200, wide - 70, 700))
 
 
 def parse_args():
@@ -49,7 +64,6 @@ def main():
 
     keyword = get_text_from_image(
         image_data=text_binary,
-        appcode=hanwan_appcode
     )
     if not keyword:
         print("text not recognize")
@@ -58,68 +72,30 @@ def main():
     keyword = keyword.split(r"．")[-1]
     keywords = keyword.split(" ")
     keyword = "".join([e.strip("\r\n") for e in keywords if e])
-    print("主人,您的问题可能是 : ", keyword)
+    print("guess keyword: ", keyword)
     answers = zhidao_search(
         keyword=keyword,
         default_answer_select=default_answer_number,
         timeout=timeout
     )
     answers = filter(None, answers)
-    for ans in answers:
-        print('='*70)
-        ans = ans.replace("\u3000", "")
-        ans = ans[0:400]
-        print("\n".join(textwrap.wrap(ans, width=45)))
+
+    for text in answers:
+        print('=' * 70)
+        text = text.replace("\u3000", "")
+        if len(text) > 120 and text_summary:
+            sentences = get_summary(text, summary_sentence_count)
+            sentences = filter(None, sentences)
+            if not sentences:
+                print(text)
+            else:
+                print("\n".join(sentences))
+        else:
+            print("\n".join(textwrap.wrap(text, width=45)))
+
     end = time.time()
     print("use {0} 秒".format(end - start))
 
 
-
-## 百度搜索
-# def main():
-#     args = parse_args()
-#     timeout = args.timeout
-
-#     start = time.time()
-
-#     c.screenshot('screenshot.png')
-#     img = Image.open('screenshot.png')
-#     wide = img.size[0]
-#     img1 = img.crop((70, 200, wide - 70, 730))
-#     img1.save('screenshot.png')
-#     text_binary = open('screenshot.png','rb').read()
-
-#     keyword = get_text_from_image(
-#         image_data=text_binary,
-#         appcode=hanwan_appcode
-#     )
-#     if not keyword:
-#         print("text not recognize")
-#         return
-#     keyword = keyword.split(r"．")[-1]
-#     keywords = keyword.split(" ")
-#     keyword = "".join([e.strip("\r\n") for e in keywords if e])
-#     print("主人,您的问题可能是 : ", keyword)
-
-
-#     results = baiduSearch.search(keyword, convey=False)
-#     print("主人,您的答案可能是 : ", results)
-#     count = 0
-#     for result in results:
-#         print('{0} {1} {2} {3} {4}'.format(result.index, result.title, result.abstract, result.show_url, result.url))  # 此处应有格式化输出
-#         print('{0}'.format(result.abstract))  # 此处应有格式化输出
-#         count=count+1
-#         if(count == 6):
-#             break
-#         end = time.time()
-#     print("use {0} 秒".format(end - start))
-
-
 if __name__ == "__main__":
     main()
-
-# fig = plt.figure()
-# pull_screenshot()
-# img = np.array(Image.open('question.png'))
-# im = plt.imshow(img, animated=True)
-
