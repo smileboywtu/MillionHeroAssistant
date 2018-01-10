16c5c76e6651fcbd0b062ce71054755f1b390eef
陈博 <chenbo@intra.nsfocus.com>
2018-01-10 16:28:21 +0800

    update config

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ default_answer_number = 2
 hanwan_appcode = "714501eede0b4ac9a75a11af64b3b4d7"
 
 ### baidu orc
-app_id = "10665467"
-app_key = "7fgQbKQ3WWtpBbwKzIdfE803"
-app_secret = " fZbPFCu4oxFciUd5nh5m6nIcbfqCzfx4"
+app_id = "10661627"
+app_key = "h5xcL0m4TB8fiiFWoysn7uxt"
+app_secret = " HGA1cgXzz80douKQUpII7K77TYWSGcfW"
 
 ### 如果你想要使用汉王的话，将汉王移动到前面，默认使用百度，每天封顶500次
 prefer = ("baidu", "hanwang")
