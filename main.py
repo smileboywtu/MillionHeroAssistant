99ff31e11778abe734a0d22939e6869c2917b613
zark <wenbo.zhang@duitang.com>
2018-01-11 17:48:28 +0800

    add ocr.space

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from config import api_version
 from config import app_id
 from config import app_key
 from config import app_secret
+from config import api_key
 from config import data_directory
 from config import default_answer_number
 from config import hanwan_appcode
@@ -27,6 +28,7 @@ from core.android import analyze_current_screen_text
 from core.baiduzhidao import zhidao_search
 from core.ocr.baiduocr import get_text_from_image as bai_get_text
 from core.ocr.hanwanocr import get_text_from_image as han_get_text
+from core.ocr.spaceocr import get_text_from_image as ocrspace_get_text
 from core.textsummary import get_summary
 
 if prefer[0] == "baidu":
@@ -39,6 +41,8 @@ if prefer[0] == "baidu":
 elif prefer[0] == "hanwang":
     get_text_from_image = partial(han_get_text, appcode=hanwan_appcode, timeout=3)
 
+elif prefer[0] == "ocrspace":
+    get_test_from_image = partial(ocrspace_get_text,api_key=api_key)
 
 def parse_args():
     parser = ArgumentParser(description="Million Hero Assistant")
