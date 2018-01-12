bbdd05418ea8f147a286a23beda52850f1d893b9
陈博 <chenbo@intra.nsfocus.com>
2018-01-12 20:05:35 +0800

    disable chrome by default

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ app_id = "10661627"
 app_key = "h5xcL0m4TB8fiiFWoysn7uxt"
 app_secret = "HGA1cgXzz80douKQUpII7K77TYWSGcfW"
 
-### ocr.space 
+### ocr.space
 api_key = "6c851da45688957"
 
 ### 默认使用百度，每天封顶500次
@@ -24,4 +24,4 @@ api_key = "6c851da45688957"
 prefer = ("baidu",  "ocrspace")
 
 ### enable chrome
-enable_chrome = True
+enable_chrome = False
